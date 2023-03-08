59c785e7c3fe906045f119405ac9164c5a1aa92a
lakshmanK1 <laxmankamisetty1@gmail.com>
2023-03-08 12:40:35 +0530

    completed search bar feature

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,14 +1,19 @@
-import React,{useState, useEffect, Fragment} from 'react'
+import React,{useState, useEffect} from 'react'
 import { RawData } from './RawData'
 import { MainDiv, SearchDiv, SearchInput, ItemsDiv, ListDiv, H3, P} from './StyledCmp'
 
 function Search() {
     const [search, setSearch] = useState('');
+    const [filteredArr, setFilteredArr] = useState([]);
+
+    useEffect(()=>{
+        let FilteredData = RawData.filter(item => item.title.toLowerCase().includes(search.toLowerCase()));
+        setFilteredArr(FilteredData);
+    },[search]);
 
     const InputVal = (e) => {
         setSearch(e.target.value);
     }
-
 
 
   return (
@@ -18,7 +23,7 @@ function Search() {
         </SearchDiv>
         <ItemsDiv>
             {
-                RawData.map((data)=>(
+                filteredArr.map((data)=>(
                     <ListDiv key={data.id}>
                         <H3>{data.title}</H3>
                         <P>{data.description}</P>
