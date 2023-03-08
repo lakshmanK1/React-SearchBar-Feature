2be6d7499c22ddc5920ef5f267121a5f39c306c7
lakshmanK1 <laxmankamisetty1@gmail.com>
2023-03-08 12:28:16 +0530

    created the list of data

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,6 +1,6 @@
 import React,{useState, useEffect, Fragment} from 'react'
 import { RawData } from './RawData'
-import { MainDiv, SearchDiv, SearchInput } from './StyledCmp'
+import { MainDiv, SearchDiv, SearchInput, ItemsDiv, ListDiv, H3, P} from './StyledCmp'
 
 function Search() {
     const [search, setSearch] = useState('');
@@ -9,11 +9,23 @@ function Search() {
         setSearch(e.target.value);
     }
 
+
+
   return (
     <MainDiv>
         <SearchDiv>
             <SearchInput type='search' value={search} onChange={InputVal} placeholder='search here..'/>
         </SearchDiv>
+        <ItemsDiv>
+            {
+                RawData.map((data)=>(
+                    <ListDiv key={data.id}>
+                        <H3>{data.title}</H3>
+                        <P>{data.description}</P>
+                    </ListDiv>
+                ))
+            }
+        </ItemsDiv>
     </MainDiv>
   )
 }
--- src/Components/StyledCmp.js
+++ src/Components/StyledCmp.js
@@ -7,3 +7,12 @@ export const SearchDiv = styled.div`margin:auto; width:220px; padding:45px;
 
 export const SearchInput = styled.input`width:220px; height:40px; border-radius:5px; border:1px solid white;
  background:transparent; outline:none; color:white; font-size:20px;`;
+
+ export const ItemsDiv = styled.div`padding:50px;`;
+
+ export const ListDiv = styled.div`box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;
+ padding:25px; margin-bottom:10px; border-radius:10px;`;
+
+ export const H3 = styled.h3``;
+
+ export const P = styled.p``;
